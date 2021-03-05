a62945c6080d23a9ff32e91c3484427ac07c8d50
DHN Chandan <php.chandan@gmail.com>
2021-03-05 20:55:15 +0600

    feat(cdk): :sparkles: Add cognito user pool, cognito domain and user pool client

--- cdk-cognito-react-backend/lib/cdk-cognito-react-backend-stack.ts
+++ cdk-cognito-react-backend/lib/cdk-cognito-react-backend-stack.ts
@@ -1,9 +1,57 @@
 import * as cdk from "@aws-cdk/core";
+import * as cognito from "@aws-cdk/aws-cognito";
 
 export class CdkCognitoReactBackendStack extends cdk.Stack {
 	constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
 		super(scope, id, props);
 
-		// The code that defines your stack goes here
+		// Creating cognito user pool
+		const userPool = new cognito.UserPool(this, "CdkCognitoReactUserPool", {
+			userPoolName: "cdk-cognito-react-user-pool",
+			selfSignUpEnabled: true,
+			signInAliases: {
+				username: true,
+				email: true,
+			},
+			autoVerify: { email: true },
+			signInCaseSensitive: false,
+			customAttributes: {
+				apiId: new cognito.StringAttribute({
+					minLen: 1,
+					maxLen: 256,
+					mutable: true,
+				}),
+				joinedOn: new cognito.DateTimeAttribute(),
+			},
+			passwordPolicy: {
+				minLength: 8,
+				requireLowercase: true,
+				requireUppercase: true,
+				requireDigits: true,
+				requireSymbols: true,
+				tempPasswordValidity: cdk.Duration.days(3),
+			},
+			accountRecovery: cognito.AccountRecovery.EMAIL_ONLY,
+		});
+
+		// Cognito domain name for app
+		userPool.addDomain("CdkCognitoReactUserPoolDomain", {
+			cognitoDomain: {
+				domainPrefix: "cdk-react-test",
+			},
+		});
+
+		// New app client
+		new cognito.UserPoolClient(this, "CdkCognitoReactUserPoolClient", {
+			userPoolClientName: "cdk-cognito-react-frontend",
+			userPool,
+			generateSecret: false,
+			authFlows: {
+				adminUserPassword: true,
+				userPassword: true,
+				userSrp: true,
+				custom: true,
+			},
+		});
 	}
 }
